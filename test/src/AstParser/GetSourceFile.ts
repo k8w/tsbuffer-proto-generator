39d1f344e95035ee5fc27a4273010f23635e1885
k8w <twoeo@qq.com>
2019-04-09 17:56:59 +0800

    flattenNodes 综合测试

--- test/src/AstParser/GetSourceFile.ts
+++ test/src/AstParser/GetSourceFile.ts
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 import * as ts from "typescript";
 import { Test } from "mocha";
+import { namespace } from './getFlattenNodes.test';
 
 export default function GetSourceFile(filename: string) {
     return ts.createSourceFile(
