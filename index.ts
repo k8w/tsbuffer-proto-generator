93e05cd6b0a882293a74e2520d5b528e8fd091f9
k8w <twoeo@qq.com>
2019-04-23 16:38:50 +0800

    index.ts export

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
 import 'k8w-extend-native';
+import SchemaGenerator from './src/SchemaGenerator';
+import EncodeIdUtil from './src/EncodeIdUtil';
+
+type TSBufferSchemaGenerator = SchemaGenerator;
+export default TSBufferSchemaGenerator;
+
+export { EncodeIdUtil };
